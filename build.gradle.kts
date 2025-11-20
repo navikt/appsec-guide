3103db3fd358b7e7ad7af93765bf91f21e52ddc2
albrektsson <emil.albrektsson@nav.no>
2025-11-20 14:03:04 +0100

    gradle: add dep bundles

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,29 +20,14 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.ktor.server.call.logging)
-    implementation(libs.ktor.server.auth)
-    implementation(libs.ktor.server.auth.jwt)
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.logback.classic)
-    implementation(libs.logstash.logback.encoder)
+    implementation(libs.bundles.ktor)
+    implementation(libs.bundles.logging)
     implementation(libs.valkey.java)
 
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.ktor.client.mock)
-    testImplementation(libs.kotlin.test.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.bundles.testing)
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly(libs.junit.platform.launcher)
-    testImplementation(libs.testcontainers)
-    testImplementation(libs.testcontainers.junit)
 }
 
 tasks.test {
